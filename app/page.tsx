c08d4362ccd450a96d8e032ee1b0546dbef510ee
yutaro hirabayashi <hirataro.yu@gmail.com>
2025-06-04 17:56:26 +0900

    add: ページ構成作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
+import Link from 'next/link';
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <Link href="/admin">Admin</Link>
+      <Link href="/about">About</Link>
+      <Link href="/admin/dashboard">Dashboard</Link>
+      <Link href="/admin/pricing">Pricing</Link>
+      <Link href="/admin/clients">Clients</Link>
+      <Link href="/admin/invoices">Invoices</Link>
+      <Link href="/admin/settings">Settings</Link>
     </>
   );
 }
